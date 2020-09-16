a8175904ac3612e02f63e0097d404d6c7c9ea3be
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-09-16 16:31:07 -0500

    adding index with 1st prompt question

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const inquirer = require("inquirer");
+
+//Need correct path to grab js file
+
+const questions = [{
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+
+]
