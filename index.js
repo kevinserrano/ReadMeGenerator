6334f01fd786a532e391b7b30b13a15f1f6e0b11
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-09-16 16:42:10 -0500

    added the description installation and usage questions

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ const inquirer = require("inquirer");
 
 const questions = [{
         type: "input",
-        name: "username",
-        message: "What is your GitHub username?"
+        name: "title",
+        message: "What is the title of your project?"
     },
+    {
+        type: "input",
+        name: "description",
+        message: "Give me a description of your project"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How does the user install your application?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How does the user use the application?"
+    },
+
 
 ]
